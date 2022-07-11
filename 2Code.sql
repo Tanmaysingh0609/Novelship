5d1e28f33067e2ddf4aa5300370ebcb7eef6b589
Tanmaysingh0609 <40896139+Tanmaysingh0609@users.noreply.github.com>
2022-07-11 13:23:49 +0530

    Update 2Code.sql

--- 2Code.sql
+++ 2Code.sql
@@ -7,6 +7,6 @@ having count(*) > 1
 
 Deliverable D: Write a query to list every entry in car table and its associated company owner. If an entry doesn't have an owner in the company table, then print 'not found' in the cell. 
 
-select *, coalesce(comp.name , 'not_found')  from car c
+select c.id ,c.make ,c.color,c.owner , coalesce(comp.name , 'not_found')  from car c
 left join company comp 
 on c.id = comp.id
